1acfef604c25dcb5930931dee8bc22b83360d1cf
krishna-28131 <krishnapurohit417400@gmail.com>
2025-12-16 19:48:57 +0530

    Add Navbar, Content, Footer and MainPage components

---
+++ Q2-Multi-Component Page/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{ color: "gray", fontSize: "14px", textAlign: "center" }}>
+      <h2>This is Footer</h2>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ Q2-Multi-Component Page/src/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+  return (
+    <div style={{ backgroundColor: "lightblue", padding: "10px" }}>
+      <h2>This is Navbar</h2>
+    </div>
+  );
+}
+
+export default Navbar;
